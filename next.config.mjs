3913f411cd27a0f12b2ce58a0d4e6367fc59bc0e
ProBimkim <bimokimi@gmail.com>
2026-09-03 15:34:30 +0700

    fix(config): add puppeteer-extra to serverExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const securityHeaders = [
 ];
 
 const nextConfig = {
-  serverExternalPackages: ['puppeteer', 'puppeteer-core', '@sparticuz/chromium-min'],
+  serverExternalPackages: ['puppeteer', 'puppeteer-core', '@sparticuz/chromium-min', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth'],
   async headers() {
     return [
       {
